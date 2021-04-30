a0a9eada3724838b4bbd96141854ddd222d503c6
Andy Kipp <anki-code@users.noreply.github.com>
2021-04-30 20:21:21 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+import setuptools
+setuptools.setup(
+    name="rever-activity-appimage-test",
+    version="42.1.1",
+    description="Rever appimage activity test",
+    url="https://github.com/regrp/rever",
+    python_requires='>=3.6',
+    install_requires=[],
+    package_data={'dir': ['*.py']},
+    packages=setuptools.find_packages(),
+    author="anki-code",
+    author_email="anki-code@example.com"
+)
